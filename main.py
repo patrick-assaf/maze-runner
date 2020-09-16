902d22dea8cb73ed97062e94ef3c9e2a1e064bea
Patrick Assaf <patrickassaf96@gmail.com>
2020-09-16 10:26:14 -0400

    Parse input text file header

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 
 file = open("input1.txt", "r")
-output = open("output.txt", "w")
+#output = open("output.txt", "w")
 
-for line in file:
-    print(line)
-    output.write(line)
+for i, line in enumerate(file):
+    if i == 0:
+        print("Algorithm used: ", line)
+    
+    elif i == 1:
+        print("Size of the maze: ", line)
+    
+    elif i == 2:
+        print("Entrance grid location: ", line)
 
-output.close()
+    elif i == 3:
+        print("Exit grid location: ", line)
+
+    elif i == 4:
+        print("Number of points where actions are available: ", line)
+    
+    else:
+        print(line)
+        #output.write(line)
+
+#output.close()
