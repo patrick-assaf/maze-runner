77c6b16d515a68313996d4164b0891b0b9c671af
Patrick Assaf <patrickassaf96@gmail.com>
2020-09-18 12:25:30 -0400

    Fill the adjacency list using the different action codes

--- main.py
+++ main.py
@@ -21,6 +21,41 @@ for i in range(header, size+header):
     adj_list[point] = []
 
     for move in range(3, line_len):
-        adj_list[point].append(line[move])
+        if line[move] == "1":
+            adj_list[point].append(str(int(line[0])+1) + " " + line[1] + " " + line[2])
+        elif line[move] == "2":
+            adj_list[point].append(str(int(line[0])-1) + " " + line[1] + " " + line[2])
+        elif line[move] == "3":
+            adj_list[point].append(line[0] + " " + str(int(line[1])+1) + " " + line[2])
+        elif line[move] == "4":
+            adj_list[point].append(line[0] + " " + str(int(line[1])-1) + " " + line[2])
+        elif line[move] == "5":
+            adj_list[point].append(line[0] + " " + line[1] + " " + str(int(line[2])+1))
+        elif line[move] == "6":
+            adj_list[point].append(line[0] + " " + line[1] + " " + str(int(line[2])-1))
+        elif line[move] == "7":
+            adj_list[point].append(str(int(line[0])+1) + " " + str(int(line[1])+1) + " " + line[2])
+        elif line[move] == "8":
+            adj_list[point].append(str(int(line[0])+1) + " " + str(int(line[1])-1) + " " + line[2]) 
+        elif line[move] == "9":
+            adj_list[point].append(str(int(line[0])-1) + " " + str(int(line[1])+1) + " " + line[2])
+        elif line[move] == "10":
+            adj_list[point].append(str(int(line[0])-1) + " " + str(int(line[1])-1) + " " + line[2])
+        elif line[move] == "11":
+            adj_list[point].append(str(int(line[0])+1) + " " + line[1] + " " + str(int(line[2])+1))
+        elif line[move] == "12":
+            adj_list[point].append(str(int(line[0])+1) + " " + line[1] + " " + str(int(line[2])-1))
+        elif line[move] == "13":
+            adj_list[point].append(str(int(line[0])-1) + " " + line[1] + " " + str(int(line[2])+1))
+        elif line[move] == "14":
+            adj_list[point].append(str(int(line[0])-1) + " " + line[1] + " " + str(int(line[2])-1))
+        elif line[move] == "15":
+            adj_list[point].append(line[0] + " " + str(int(line[1])+1) + " " + str(int(line[2])+1))
+        elif line[move] == "16":
+            adj_list[point].append(line[0] + " " + str(int(line[1])+1) + " " + str(int(line[2])-1))
+        elif line[move] == "17":
+            adj_list[point].append(line[0] + " " + str(int(line[1])-1) + " " + str(int(line[2])+1))
+        elif line[move] == "18":
+            adj_list[point].append(line[0] + " " + str(int(line[1])-1) + " " + str(int(line[2])-1))
 
 print(adj_list)
