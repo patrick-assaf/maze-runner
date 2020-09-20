fba0f4955dc3c3a872cc429e6207f41802872a71
Patrick Assaf <patrickassaf96@gmail.com>
2020-09-19 20:50:23 -0400

    Add edge costs when computing graph for UCS or A*

--- main.py
+++ main.py
@@ -18,8 +18,16 @@ header = 5
 
 adj_list = {}
 
-def create_adj_list():
+def append_value(code):
+    if algorithm == "BFS":
+        return ""
+    elif algorithm == "UCS" or algorithm == "A*":
+        if code < 7:
+            return " 10" 
+        elif code >= 7:
+            return " 14"  
 
+def create_adj_list():
     for i in range(header, size+header):
         line = lines[i].rstrip("\n").split()
         point = line[0] + " " + line[1] + " " + line[2]
@@ -30,44 +38,43 @@ def create_adj_list():
 
         for move in range(3, line_len):
             if line[move] == "1":
-                adj_list[point].append(str(int(line[0])+1) + " " + line[1] + " " + line[2])
+                adj_list[point].append(str(int(line[0])+1) + " " + line[1] + " " + line[2] + append_value(1))
             elif line[move] == "2":
-                adj_list[point].append(str(int(line[0])-1) + " " + line[1] + " " + line[2])
+                adj_list[point].append(str(int(line[0])-1) + " " + line[1] + " " + line[2] + append_value(2))
             elif line[move] == "3":
-                adj_list[point].append(line[0] + " " + str(int(line[1])+1) + " " + line[2])
+                adj_list[point].append(line[0] + " " + str(int(line[1])+1) + " " + line[2] + append_value(3))
             elif line[move] == "4":
-                adj_list[point].append(line[0] + " " + str(int(line[1])-1) + " " + line[2])
+                adj_list[point].append(line[0] + " " + str(int(line[1])-1) + " " + line[2] + append_value(4))
             elif line[move] == "5":
-                adj_list[point].append(line[0] + " " + line[1] + " " + str(int(line[2])+1))
+                adj_list[point].append(line[0] + " " + line[1] + " " + str(int(line[2])+1) + append_value(5))
             elif line[move] == "6":
-                adj_list[point].append(line[0] + " " + line[1] + " " + str(int(line[2])-1))
+                adj_list[point].append(line[0] + " " + line[1] + " " + str(int(line[2])-1) + append_value(6))
             elif line[move] == "7":
-                adj_list[point].append(str(int(line[0])+1) + " " + str(int(line[1])+1) + " " + line[2])
+                adj_list[point].append(str(int(line[0])+1) + " " + str(int(line[1])+1) + " " + line[2] + append_value(7))
             elif line[move] == "8":
-                adj_list[point].append(str(int(line[0])+1) + " " + str(int(line[1])-1) + " " + line[2]) 
+                adj_list[point].append(str(int(line[0])+1) + " " + str(int(line[1])-1) + " " + line[2] + append_value(8)) 
             elif line[move] == "9":
-                adj_list[point].append(str(int(line[0])-1) + " " + str(int(line[1])+1) + " " + line[2])
+                adj_list[point].append(str(int(line[0])-1) + " " + str(int(line[1])+1) + " " + line[2] + append_value(9))
             elif line[move] == "10":
-                adj_list[point].append(str(int(line[0])-1) + " " + str(int(line[1])-1) + " " + line[2])
+                adj_list[point].append(str(int(line[0])-1) + " " + str(int(line[1])-1) + " " + line[2] + append_value(10))
             elif line[move] == "11":
-                adj_list[point].append(str(int(line[0])+1) + " " + line[1] + " " + str(int(line[2])+1))
+                adj_list[point].append(str(int(line[0])+1) + " " + line[1] + " " + str(int(line[2])+1) + append_value(11))
             elif line[move] == "12":
-                adj_list[point].append(str(int(line[0])+1) + " " + line[1] + " " + str(int(line[2])-1))
+                adj_list[point].append(str(int(line[0])+1) + " " + line[1] + " " + str(int(line[2])-1) + append_value(12))
             elif line[move] == "13":
-                adj_list[point].append(str(int(line[0])-1) + " " + line[1] + " " + str(int(line[2])+1))
+                adj_list[point].append(str(int(line[0])-1) + " " + line[1] + " " + str(int(line[2])+1) + append_value(13))
             elif line[move] == "14":
-                adj_list[point].append(str(int(line[0])-1) + " " + line[1] + " " + str(int(line[2])-1))
+                adj_list[point].append(str(int(line[0])-1) + " " + line[1] + " " + str(int(line[2])-1) + append_value(14))
             elif line[move] == "15":
-                adj_list[point].append(line[0] + " " + str(int(line[1])+1) + " " + str(int(line[2])+1))
+                adj_list[point].append(line[0] + " " + str(int(line[1])+1) + " " + str(int(line[2])+1) + append_value(15))
             elif line[move] == "16":
-                adj_list[point].append(line[0] + " " + str(int(line[1])+1) + " " + str(int(line[2])-1))
+                adj_list[point].append(line[0] + " " + str(int(line[1])+1) + " " + str(int(line[2])-1) + append_value(16))
             elif line[move] == "17":
-                adj_list[point].append(line[0] + " " + str(int(line[1])-1) + " " + str(int(line[2])+1))
+                adj_list[point].append(line[0] + " " + str(int(line[1])-1) + " " + str(int(line[2])+1) + append_value(17))
             elif line[move] == "18":
-                adj_list[point].append(line[0] + " " + str(int(line[1])-1) + " " + str(int(line[2])-1))
+                adj_list[point].append(line[0] + " " + str(int(line[1])-1) + " " + str(int(line[2])-1) + append_value(18))
 
 def use_bfs(adjacency_list, start_node, end_node):
-
     visited_nodes = set()
     queue = deque([[start_node]])
 
@@ -107,11 +114,10 @@ def use_bfs(adjacency_list, start_node, end_node):
     output.write("FAIL")
 
 def use_ucs():
-
-    output.write("TODO: UCS")
+    create_adj_list()
+    print(adj_list)
 
 def use_astar():
-
     output.write("TODO: A*")
 
 if algorithm == "BFS":
